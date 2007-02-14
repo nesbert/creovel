6a86384b1a4f57c9ff07719b7235ea802a253d70
nesbert <nesbert@ef674ccf-a420-0410-bc6a-75797cd24154>
2007-02-14 06:57:18 +0000

    More documention and updated error message for people trying to vew the source when $_ENV[view_source] is not set in the config/environment.php file.

--- vendor/creovel/classes/error.php
+++ vendor/creovel/classes/error.php
@@ -1,7 +1,8 @@
 <?php
 /*
 
-Class: error
+	Class: error
+	
 	Error handler for Creovel.
 
 */
@@ -9,135 +10,118 @@ Class: error
 class error
 {
 
-	private $type;
-	private $error_count = 0;
-
 	// Section: Public
 
 	/*
 	
-	Function: __construct
+		Function: __construct
+		
 		Set error type in construct.
-
-	Parameters:
-		type - applicaiton, model
-
+		
+		Parameters:
+		
+			type - Applicaiton or model error.
 	*/
 
 	public function __construct($type)
 	{
-		$this->type = $type;
+		$this->_type = $type;
 	}
 	
 	/*
-
-	Function: add			
+	
+		Function: add
+		
 		Add errors to object
-
-	Parameters:	
-		args[0] - message if application error and field name if model error
-		object|string $args[1] - exception oject if application error and message if model error
-
+		
+		Parameters:
+		
+			args[0] - Message if application error and field name if model error.
+			object|string $args[1] - Exception oject for application error and message if model error.
+	
 	*/
 
 	public function add()
 	{
-		$this->error_count++;
+		$this->_error_count++;
 		$args = func_get_args();
 		
-		switch ( $this->type ) {
+		switch ( $this->_type ) {
 		
 			case 'application':
-				$this->application_error($args[0], $args[1]);
+				$this->_application_error($args[0], $args[1]);
 			break;
 		
 			default:
-				$this->model_error($args[0], $args[1]);
+				$this->_model_error($args[0], $args[1]);
 			break;
 		
 		}	
 	}
 	
 	/*
-
-	Function:	
-		Returns bool value for errors
-
-	Returns:	
-		bool
+	
+		Function: has_errors
+		
+		Returns bool value for errors.
+		
+		Returns:	
+		
+		Bool.
 
 	*/
 
 	public function has_errors()
 	{
-		return $this->error_count ? true : false;
+		return $this->_error_count ? true : false;
 	}
 	
 	/*
 
-	Function:		
-		Accessor for Error Count
+		Function: count
+		
+		Accessor for error count.
 
-	Return:	
-		Int
+		Return:
+		
+		Integer of the total errors.
+	
 	*/
 
 	public function count()
 	{
-		return $this->error_count;
+		return $this->_error_count;
 	}
 	
-	/*
-	
-	Function:	
-		Display application errors to user
-
-	Parameters:
-		message - required
-		exception - optional
-
-	*/
-
-	private function application_error($message, $exception = null)
-	{
-		// check whether or not to show debugging errors
-		$this->handle_error();
-		
-		// clean output buffer for application errors
-		@ob_end_clean();
-		
-		$this->message = $message;
-		
-		if ( is_object($exception) ) $this->traces = $exception->getTrace();
-		
-		if ( isset($_GET['view_source']) ) {
-			if ( $_ENV['view_source'] && strstr($_GET['view_source'], BASE_PATH) ) {		
-				view::_show_view(CREOVEL_PATH.'views'.DS.'view_source.php', CREOVEL_PATH.'views'.DS.'layouts'.DS.'creovel.php');
-			} else {
-				die('Looking for something? <a href="creovel.org">creovel.org</a>');
-			}
-		} elseif (isset($_SERVER['argv'][0])) {
-			view::_show_view(CREOVEL_PATH.'views'.DS.'command_line_error.php', CREOVEL_PATH.'views'.DS.'layouts'.DS.'command_line.php');
-		} else {
-			view::_show_view(CREOVEL_PATH.'views'.DS.'application_error.php', CREOVEL_PATH.'views'.DS.'layouts'.DS.'creovel.php');
-		}
-		
-		die;
-	}
-
 	// Section: Private
-
+	
 	/*
-			
-	Function: handle_error
+		Property: _type
+		Error type either application or model error.
+	*/
+	
+	private $_type;
+	
+	/*
+		Property: _error_count
+		Number of error in this object.
+	*/
+	
+	private $_error_count = 0;
+	
+	/*
+	
+		Function: _handle_error
+		
 		Handle how to display errors to the user. If in dvelopment mode show debugging information else redirect to error page.
-
-	Returns:
-		bool
+		
+		Returns:
+		
+		Bool or shows error page.
 
 	*/
 
-	private function handle_error()
+	private function _handle_error()
 	{
 		if ( $_ENV['mode'] != 'development' ) {
 			switch ( true )
@@ -158,22 +142,64 @@ class error
 	}
 	
 	/*
-
-	Function: model_error	
-		Create a property for each error
-
-	Paramenters:	
-		field - required
-		message - required
-
+	
+		Function: _model_error
+		
+		Create a property for each error.
+		
+		Paramenters:
+		
+			field - Required string of field name
+			message - Required string of error message for field.
+	
 	*/
 
-	private function model_error($field, $message)
+	private function _model_error($field, $message)
 	{
 		$this->$field = $message;
 		// add to globals
 		$_ENV['model_error'][$field] = $message;
-	}	
+	}
 	
+	/*
+	
+		Function:_application_error
+		
+		Display application errors to user.
+
+		Parameters:
+			
+			message - Required error string.
+			exception - Optional bool. If set to true sets traces for debugger.
+
+	*/
+
+	private function _application_error($message, $exception = null)
+	{
+		// check whether or not to show debugging errors
+		$this->_handle_error();
+		
+		// clean output buffer for application errors
+		@ob_end_clean();
+		
+		$this->message = $message;
+		
+		if ( is_object($exception) ) $this->traces = $exception->getTrace();
+		
+		if ( isset($_GET['view_source']) ) {
+			if ( $_ENV['view_source'] && strstr($_GET['view_source'], BASE_PATH) ) {		
+				view::_show_view(CREOVEL_PATH.'views'.DS.'view_source.php', CREOVEL_PATH.'views'.DS.'layouts'.DS.'creovel.php');
+			} else {
+				header("HTTP/1.0 404 Not Found");
+				die('$_ENV[\'view_source\'] must be set in your config file. For more information visit <a href="http://www.creovel.org">http://www.creovel.org</a>.');
+			}
+		} elseif (isset($_SERVER['argv'][0])) {
+			view::_show_view(CREOVEL_PATH.'views'.DS.'command_line_error.php', CREOVEL_PATH.'views'.DS.'layouts'.DS.'command_line.php');
+		} else {
+			view::_show_view(CREOVEL_PATH.'views'.DS.'application_error.php', CREOVEL_PATH.'views'.DS.'layouts'.DS.'creovel.php');
+		}
+		
+		die;
+	}
 }
 ?>

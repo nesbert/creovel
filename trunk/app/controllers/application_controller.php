956ee8bfa36ccbc5b75a23f54ce938f7619ebc82
russ816 <russ816@ef674ccf-a420-0410-bc6a-75797cd24154>
2007-02-24 02:16:24 +0000

    User docs can now be generated. I hope we can find a cleaner way to do this next time. Thast is the most ugly bash script I have *ever* written.

--- trunk/app/controllers/application_controller.php
+++ trunk/app/controllers/application_controller.php
@@ -1,6 +1,16 @@
 <?php
+
+/*
+
+Script: application_controller
+	Place Application wide code here.
+	Like before filters and after filters.
+
+*/
+
 class application_controller extends controller
 {
 
 }
+
 ?>
--- trunk/app/controllers/index_controller.php
+++ trunk/app/controllers/index_controller.php
@@ -1,16 +1,37 @@
 <?php
+
+/*
+
+Script: index_controller
+	Default Controller
+
+*/
+
 class index_controller extends application_controller
 {
+	/*
+	
+	Function: index
+		Default Action
+
+	*/
 
 	public function index()
 	{
 		$this->say_hello();
 	}
+
+	/*
 	
+	Function: say_hello
+		Sample Function
+
+	*/	
+
 	private function say_hello()
 	{
 		$this->render_text = "<p>$ Hello World!</p>";
 	}
-
 }
+
 ?>

ee4a9a64811fcd15a64f808beaba20655afbbed7
russ816 <russ816@ef674ccf-a420-0410-bc6a-75797cd24154>
2007-02-28 21:49:33 +0000

    Updated to add more word variations. I'm sure more are coming soon.

--- trunk/vendor/creovel/classes/inflector.php
+++ trunk/vendor/creovel/classes/inflector.php
@@ -32,34 +32,45 @@ class inflector
 
     public function pluralize($word)
     {
-        $plural = array(
-        '/(quiz)$/i' => '\1zes',
-        '/^(ox)$/i' => '\1en',
-        '/([m|l])ouse$/i' => '\1ice',
-        '/(matr|vert|ind)ix|ex$/i' => '\1ices',
-        '/(x|ch|ss|sh)$/i' => '\1es',
-        '/([^aeiouy]|qu)ies$/i' => '\1y',
-        '/([^aeiouy]|qu)y$/i' => '\1ies',
-        '/(hive)$/i' => '\1s',
-        '/(?:([^f])fe|([lr])f)$/i' => '\1\2ves',
-        '/sis$/i' => 'ses',
-        '/([ti])um$/i' => '\1a',
-        '/(buffal|tomat)o$/i' => '\1oes',
-        '/(bu)s$/i' => '\1ses',
-        '/(alias|status)/i'=> '\1es',
-        '/(octop|vir)us$/i'=> '\1i',
-        '/(ax|test)is$/i'=> '\1es',
-        '/s$/i'=> 's',
-        '/$/'=> 's');
+		$plural = array
+		(
+			'/(n)ews$/i' => '\1ews',
+			'/([ti])a$/i' => '\1um',
+			'/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+			'/(^analy)ses$/i' => '\1sis',
+			'/([^f])ves$/i' => '\1fe',
+			'/(hive)s$/i' => '\1',
+			'/(tive)s$/i' => '\1',
+			'/([lr])ves$/i' => '\1f',
+			'/([^aeiouy]|qu)ies$/i' => '\1y',
+			'/(s)eries$/i' => '\1eries',
+			'/(m)ovies$/i' => '\1ovie',
+			'/(x|ch|ss|sh)es$/i' => '\1',
+			'/([m|l])ice$/i' => '\1ouse',
+			'/(bus)es$/i' => '\1',
+			'/(o)es$/i' => '\1',
+			'/(shoe)s$/i' => '\1',
+			'/(cris|ax|test)es$/i' => '\1is',
+			'/(octop|vir)i$/i' => '\1us',
+			'/(alias|status)es$/i' => '\1',
+			'/^(ox)en/i' => '\1',
+			'/(vert|ind)ices$/i' => '\1ex',
+			'/(matr)ices$/i' => '\1ix',
+			'/(quiz)zes$/i' => '\1',
+			'/s$/i' => 's',
+			'/$/' => 's'
+		);
 
         $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
 
-        $irregular = array(
-        'person' => 'people',
-        'man' => 'men',
-        'child' => 'children',
-        'sex' => 'sexes',
-        'move' => 'moves');
+		$irregular = array
+		(
+			'person' => 'people',
+			'man' => 'men',
+			'child' => 'children',
+			'sex' => 'sexes',
+			'move' => 'moves'
+		);
 
         $lowercased_word = strtolower($word);
 
@@ -99,31 +110,32 @@ class inflector
 
     public function singularize($word)
     {
-        $singular = array (
-        '/(quiz)zes$/i' => '\\1',
-        '/(matr)ices$/i' => '\\1ix',
-        '/(vert|ind)ices$/i' => '\\1ex',
-        '/^(ox)en/i' => '\\1',
-        '/(alias|status)es$/i' => '\\1',
-        '/([octop|vir])i$/i' => '\\1us',
-        '/(cris|ax|test)es$/i' => '\\1is',
-        '/(shoe)s$/i' => '\\1',
-        '/(o)es$/i' => '\\1',
-        '/(bus)es$/i' => '\\1',
-        '/([m|l])ice$/i' => '\\1ouse',
-        '/(x|ch|ss|sh)es$/i' => '\\1',
-        '/(m)ovies$/i' => '\\1ovie',
-        '/(s)eries$/i' => '\\1eries',
-        '/([^aeiouy]|qu)ies$/i' => '\\1y',
-        '/([lr])ves$/i' => '\\1f',
-        '/(tive)s$/i' => '\\1',
-        '/(hive)s$/i' => '\\1',
-        '/([^f])ves$/i' => '\\1fe',
-        '/(^analy)ses$/i' => '\\1sis',
-        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\\1\\2sis',
-        '/([ti])a$/i' => '\\1um',
-        '/(n)ews$/i' => '\\1ews',
-        '/s$/i' => '',
+		$singular = array
+		(
+			'/(n)ews$/i' => '\1ews',
+			'/([ti])a$/i' => '\1um',
+			'/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+			'/(^analy)ses$/i' => '\1sis',
+			'/([^f])ves$/i' => '\1fe',
+			'/(hive)s$/i' => '\1',
+			'/(tive)s$/i' => '\1',
+			'/([lr])ves$/i' => '\1f',
+			'/([^aeiouy]|qu)ies$/i' => '\1y',
+			'/(s)eries$/i' => '\1eries',
+			'/(m)ovies$/i' => '\1ovie',
+			'/(x|ch|ss|sh)es$/i' => '\1',
+			'/([m|l])ice$/i' => '\1ouse',
+			'/(bus)es$/i' => '\1',
+			'/(o)es$/i' => '\1',
+			'/(shoe)s$/i' => '\1',
+			'/(cris|ax|test)es$/i' => '\1is',
+			'/(octop|vir)i$/i' => '\1us',
+			'/(alias|status)es$/i' => '\1',
+			'/^(ox)en/i' => '\1',
+			'/(vert|ind)ices$/i' => '\1ex',
+			'/(matr)ices$/i' => '\1ix',
+			'/(quiz)zes$/i' => '\1',
+			'/s$/i' => '',
         );
 
         $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');

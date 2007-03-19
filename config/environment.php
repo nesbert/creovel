a21e01c4385b0908387cb542b7f94021858616f8
nesbert <nesbert@ef674ccf-a420-0410-bc6a-75797cd24154>
2007-03-19 03:09:59 +0000

    Organized and group stuff... environment.php now only contains env settings and added database & paths files.

--- config/environment.php
+++ config/environment.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 
 	Scripts: environment
@@ -11,58 +12,25 @@
 
 */
 
-// set application mode
-$_ENV['mode'] = 'production'; // development, test, production
+// Set application mode.
+$_ENV['mode'] = 'development'; // development, test, production
 
-// set development database properties
-$_ENV['development']['adapter']		= 'mysql';
-$_ENV['development']['host']		= 'localhost';
-$_ENV['development']['database']	= 'database';
-$_ENV['development']['username']	= 'username';
-$_ENV['development']['password']	= 'password';
-
-// set test database properties
-$_ENV['test']['adapter']			= 'mysql';
-$_ENV['test']['host']				= 'localhost';
-$_ENV['test']['database']			= '';
-$_ENV['test']['username']			= '';
-$_ENV['test']['password']			= '';
-
-// set production database properties
-$_ENV['production']['adapter']		= 'mysql';
-$_ENV['production']['host']			= 'localhost';
-$_ENV['production']['database']		= '';
-$_ENV['production']['username']		= '';
-$_ENV['production']['password']		= '';
-
-// email application errors when not in development mode
+// Email application errors when not in development mode (set to enable).
 $_ENV['email_errors'] = 'youremail@yourdomain.com'; // use commas for multiple email addresses
 
-// set session handler
+// Set session handler.
 $_ENV['sessions'] = true; // false, true, 'table'
 
 // show source in debugger for all files
 $_ENV['view_source'] = false;
 
-// define application urls
-define(BASE_URL, 			'http'.( $_SERVER['HTTPS'] == "on" ? 's' : '' ).'://'.$_SERVER['HTTP_HOST']);
-define(CSS_URL,				BASE_URL.'/stylesheets/');
-define(JAVASCRIPT_URL,		BASE_URL.'/javascripts/');
+// Include database configration settings.
+require_once 'database.php';
 
-// define application paths
-define(BASE_PATH, 			dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
-define(CONFIG_PATH, 		BASE_PATH.'config'.DIRECTORY_SEPARATOR);
-define(PUBLIC_PATH, 		BASE_PATH.'public'.DIRECTORY_SEPARATOR);
-define(APP_PATH, 			BASE_PATH.'app'.DIRECTORY_SEPARATOR);
-define(MODELS_PATH, 		APP_PATH.'models'.DIRECTORY_SEPARATOR);
-define(VIEWS_PATH, 			APP_PATH.'views'.DIRECTORY_SEPARATOR);
-define(CONTROLLERS_PATH, 	APP_PATH.'controllers'.DIRECTORY_SEPARATOR);
-define(HELPERS_PATH, 		APP_PATH.'helpers'.DIRECTORY_SEPARATOR);
-define(SCRIPT_PATH, 		BASE_PATH.'script'.DIRECTORY_SEPARATOR);
-#define(SHARED_PATH, 		BASE_PATH.'shared'.DIRECTORY_SEPARATOR); // use/modify when needed
-define(VENDOR_PATH, 		BASE_PATH.'vendor'.DIRECTORY_SEPARATOR);
-define(CREOVEL_PATH, 		VENDOR_PATH.'creovel'.DIRECTORY_SEPARATOR);
+// Include application paths.
+require_once 'paths.php';
 
-// initialize framework and include core libraries
-require_once(CREOVEL_PATH.'initialize.php');
+// Initialize framework and include core libraries.
+require_once CREOVEL_PATH.'initialize.php';
+
 ?>

eb92bdb5df6c36e83626e2454c32c7aa9e491eef
nesbert <nesbert@ef674ccf-a420-0410-bc6a-75797cd24154>
2008-11-15 00:39:50 +0000

    Spartacus: Form errors view.

---
+++ branches/spartacus/vendor/creovel/views/layouts/_form_errors.php
@@ -0,0 +1,11 @@
+<div class="errors">
+<div class="top"></div>
+<div class="body">
+<?php echo($title ? '<h1 class="error_title">'.$title.'</h1>' : ''); ?>
+<?php echo($description ? '<p>'.$description.'</p>' : ''); ?>
+<ul>
+<?php echo $li_str; ?>
+</ul>
+</div>
+<div class="bottom"></div>
+</div>

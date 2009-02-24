76077b653ebe2e603248881a5ce51db825539629
nesbert <nesbert@ef674ccf-a420-0410-bc6a-75797cd24154>
2009-02-24 08:12:43 +0000

    Removing old trunk... changes are to drastic to merge spartacus into trunk properly.

--- trunk/app/views/index/error.php
+++
@@ -1,3 +0,0 @@
-<h1>Custom Error</h1>
-<p>Sorry, an error occurred while processing your last request.</p>
-<p><?=$this->params['error']->getMessage()?></p>
--- trunk/vendor/creovel/views/view_source.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-	Layout view used by creovel to display application source information.
-*/
-?>
-<h1 class="top">View Source</h1>
-<p class="top"><?=$trace['file'] = $_GET['view_source']?> (<?=get_filesize($_GET['view_source'])?>)</p>
-<?php include dirname(__FILE__).DS.'_source_code.php' ?>
